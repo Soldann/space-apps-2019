6094f1d1498d15572d40116af87349f5c828cc68
Han <hasd@gm>
2019-10-19 15:05:41 -0400

    Moved code into method for years and numbers

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import matplotlib.pyplot as plt
 from astropy.utils.data import get_pkg_data_filename
 import os
 
-directory = "NEOSSAT/ASTRO/2019/284/"
 
-for filename in os.listdir(directory):
-    if filename.endswith("clean.fits"): 
-        image_file = get_pkg_data_filename(directory  + filename)
-        image_data = fits.getdata(image_file, ext=0)
-        plt.figure()
-        plt.imshow(image_data, cmap='gray')
-        plt.colorbar()
-        print(filename)
-        #plt.close()
+def read_dirs(year, numbers):
+    for number in numbers:
+        directory = "NEOSSAT/ASTRO/" + year +"/" + number
+    
+        for filename in os.listdir(directory):
+            if filename.endswith("clean.fits"): 
+                image_file = get_pkg_data_filename(directory  + filename)
+                image_data = fits.getdata(image_file, ext=0)
+                plt.figure()
+                plt.imshow(image_data, cmap='gray')
+                plt.colorbar()
+                print(filename)
+                #plt.close()
 
 #image_file = get_pkg_data_filename('NEOSSAT/ASTRO/2019/284/NEOS_SCI_2019284114500.fits')
