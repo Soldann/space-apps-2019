c090a9eea0c11a35452feacbda761cc00bb0d9da
Landson <guotl321@gmail.com>
2019-10-19 14:50:19 -0400

    opencv usage

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from astropy.io import fits
+import cv2
 
 img = fits.open('NEOSSAT/ASTRO/2019/284/NEOS_SCI_2019284114500.fits')
-
 print("Hello world")
+cv2.imshow('image',img[0].data)
 
 
 img.close()
