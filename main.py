13fbe3e330b54d5d0c96fbeed6ccd7c4d3715681
Han <hasd@gm>
2019-10-19 15:30:22 -0400

    WIP no longer crashes but does not generate video

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from astropy.utils.data import get_pkg_data_filename
 import os
 import matplotlib.cm as cm
 import subprocess
+import glob
+
 cwd = os.getcwd()
 
 def read_dirs(year, numbers):
@@ -29,10 +31,12 @@ def generate_video(img):
         plt.imshow(img[i], cmap=cm.Greys_r)
         plt.savefig(cwd + "/file%02d.png" % i)
 
-    subprocess.call([
+    '''subprocess.call([
         'ffmpeg', '-framerate', '8', '-i', 'file%02d.png', '-r', '30', '-pix_fmt', 'yuv420p',
         'video_name.mp4'
-    ])
+    ])'''
+    os.system("ffmpeg -r 1 -i file%01d.png -vcodec mpeg4 -y movie.mp4")
+    print("Done subprocess")
     for file_name in glob.glob("*.png"):
         os.remove(file_name)
         
