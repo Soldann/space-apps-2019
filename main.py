06635c336a522c95a82c4d9ef72e24b4d2182f69
Han <hasd@gm>
2019-10-19 15:21:44 -0400

    Made png's

--- main.py
+++ main.py
@@ -2,27 +2,33 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 from astropy.utils.data import get_pkg_data_filename
 import os
-
+import matplotlib.cm as cm
+import subprocess
+cwd = os.getcwd()
 
 def read_dirs(year, numbers):
+    images = []
     for number in numbers:
-        directory = "NEOSSAT/ASTRO/" + str(year) +"/" + str(number)
+        directory = "NEOSSAT/ASTRO/" + str(year) +"/" + str(number) + "/"
     
         for filename in os.listdir(directory):
             if filename.endswith("clean.fits"): 
-                image_file = get_pkg_data_filename(directory  + filename)
+                image_file = get_pkg_data_filename(directory + filename)
                 image_data = fits.getdata(image_file, ext=0)
+                images.append(image_data)
+                ''' 
                 plt.figure()
                 plt.imshow(image_data, cmap='gray')
                 plt.colorbar()
                 print(filename)
+                '''
                 #plt.close()
+    return images
 def generate_video(img):
-    for i in xrange(len(img)):
+    for i in range(len(img)):
         plt.imshow(img[i], cmap=cm.Greys_r)
-        plt.savefig(folder + "/file%02d.png" % i)
+        plt.savefig(cwd + "/file%02d.png" % i)
 
-    os.chdir("your_folder")
     subprocess.call([
         'ffmpeg', '-framerate', '8', '-i', 'file%02d.png', '-r', '30', '-pix_fmt', 'yuv420p',
         'video_name.mp4'
@@ -31,5 +37,6 @@ def generate_video(img):
         os.remove(file_name)
         
 
-read_dirs(2019, [284])
+generate_video(read_dirs(2019, [284]))
+print("Done!")
 #image_file = get_pkg_data_filename('NEOSSAT/ASTRO/2019/284/NEOS_SCI_2019284114500.fits')
